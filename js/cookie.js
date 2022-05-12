cf032d5dc7da0e3248d78a40e0dbb0f32f60c3c0
Diego A. Carrillo <84059678+dcarrillo1994@users.noreply.github.com>
2022-05-12 13:38:34 -0500

    Update cookie.js

--- js/cookie.js
+++ js/cookie.js
@@ -4,8 +4,10 @@ const botonAceptarCookies = document.getElementById("btn-aceptar-cookies");
 const avisoCookies = document.getElementById("aviso-cookies");
 const fondoAvisoCookies = document.getElementById("fondo-aviso-cookies");
 
-avisoCookies.classList.add("activo");
-fondoAvisoCookies.classList.add("activo");
+if(!localStorage.getItem("cookies-aceptadas")){
+    avisoCookies.classList.add("activo");
+    fondoAvisoCookies.classList.add("activo");
+}
 
 botonAceptarCookies.addEventListener("click", () => {
     avisoCookies.classList.remove("activo");
